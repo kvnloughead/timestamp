d8ba09de0a533351682b139efc7500e2e6aa5979
Kevin Loughead <kvnloughead@gmail.com>
2021-09-04 14:36:30 -0400

    fix conversion from unix timestamp

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,9 +1,14 @@
+const isInteger = (value) => {
+  console.log('value ', value)
+  return /^\d+$/.test(value);
+}
+
 const getUnixEpoch = (t) => {
-  return new Date(t).getTime() / 1000
+  return new Date(isInteger(t) ? parseInt(t) : t).getTime();
 }
 
 const getUTCString = (t) => {
-  return new Date(t).toUTCString();
+  return new Date(isInteger(t) ? parseInt(t) : t).toUTCString();
 }
 
 module.exports = {
